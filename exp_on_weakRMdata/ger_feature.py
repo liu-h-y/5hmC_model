8d9d201fa2b75302a4377e3ce16838c6905462bf
liu-h-y <51401167+liu-h-y@users.noreply.github.com>
2022-01-31 16:38:27 +0800

    Add files via upload

---
+++ exp_on_weakRMdata/ger_feature.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+import gensim
+
+df = pd.read_csv("weakRM_data/data/train.csv")
+#df = pd.read_csv("weakRM_data/data/valid.csv")
+#df = pd.read_csv("weakRM_data/data/train.csv")
+
+embedding_vec = gensim.models.KeyedVectors.load_word2vec_format("../data/dm3.w2v")
+
+num_samples = df.shape[0]
+for i in range(num_samples):
+    feature_val = []
+    label = df.iloc[i,0]
+    seq = df.iloc[i,-2]
+    if label == 'P':
+        feature_val.append(1)
+    else:
+        feature_val.append(0)
+
+    for len_word in range(3,9):
+        vec = np.zeros(100)
+        for index in range(len(seq)-len_word+1):
+            word = seq[index:index+len_word]
+            word = word.replace('U','T')
+            vec += embedding_vec[word]
+        vec /= len(seq)-len_word+1
+        list_vec = vec.tolist()
+        feature_val.extend(list_vec)
+    df_out = pd.DataFrame(feature_val).T
+    df_out.to_csv("weakRM_data/feature/train.csv",mode='a',index=False,header=None)
+    #df_out.to_csv("weakRM_data/feature/valid.csv",mode='a',index=False,header=None)
+    #df_out.to_csv("weakRM_data/feature/test.csv",mode='a',index=False,header=None)
